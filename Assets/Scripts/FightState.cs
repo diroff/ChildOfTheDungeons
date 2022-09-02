dffa33ea6679d1d978ab36ec2b7c45f576e51735
diroff <danil.aprelskiy@mail.ru>
2022-09-03 01:34:56 +0300

    Перевел на новую версию
    Новая версия проекта (потом напишу цифрами);
    Начало работы над стейтами игры;
    Не довел работу переключения стадий до конца, потом доделать надо

---
+++ Assets/Scripts/FightState.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class FightState : MonoBehaviour
+{
+    private int _stateIndex;
+
+    private void StateIndex() { }
+}
