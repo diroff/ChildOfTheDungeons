c8704d7166f4a260de7b3bcbb84586ea6d1ab2ad
diroff <danil.aprelskiy@mail.ru>
2023-02-04 03:46:51 +0300

    refactor: remove buttons scripts
    Удалил все скрипты кнопок, поскольку теперь все необходимые действия просто накидываются на кнопку

--- Assets/Scripts/UI/Buttons/ButtonAction.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-using UnityEngine.UI;
-
-public abstract class ButtonAction : MonoBehaviour
-{
-    [SerializeField] protected Button Button;
-
-    public abstract void OnClickAction();
-}
