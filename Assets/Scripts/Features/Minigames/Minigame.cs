00371cbebf94a402b13d849c395fd3e9c0ec06c8
diroff <danil.aprelskiy@mail.ru>
2023-04-03 16:16:09 +0300

    feat: add base minigame script
    Добавил базовый скрипт для мини-игр

---
+++ Assets/Scripts/Features/Minigames/Minigame.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public abstract class Minigame : MonoBehaviour
+{
+    [SerializeField] protected GameObject GamePanel;
+
+    protected float Result;
+
+    private void OnEnable()
+    {
+        SetPanelState(true);
+    }
+
+    public void SetPanelState(bool active)
+    {
+        GamePanel.SetActive(active);
+    }
+
+    public virtual float GetGameResult()
+    {
+        return Result;
+    }
+}
